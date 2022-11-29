375ac4e2bf100ee74fc125b33f57c7cb2f6a5106
soycristhianflores <69639311+soycristhianflores@users.noreply.github.com>
2022-11-29 14:43:33 -0600

    Menu Superior Responsivo

--- vistas/plantilla/modulos/navbartop.php
+++ vistas/plantilla/modulos/navbartop.php
@@ -2,6 +2,32 @@
 	<a class="navbar-brand" href="<?php echo SERVERURL; ?>dashboard/"><center><img src="<?php echo SERVERURL; ?>vistas/plantilla/img/logo1.png" width="90%" alt="We share" loading="lazy"></center></a>
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars fa-lg"></i></button>
     <!-- Navbar Search-->
+
+        <a class="nav-link link d-none d-sm-none d-md-block reporteVentas" href="<?php echo SERVERURL; ?>reporteVentas/" id="" style="display:none"><div class="sb-nav-link-icon"></i></div>Reporte Ventas</a>
+        <a class="nav-link link d-none d-sm-none d-md-block reporteCotizacion" href="<?php echo SERVERURL; ?>reporteCotizacion/" id="" style="display:none"><div class="sb-nav-link-icon"></div>Reporte Cotización</a>
+        <a class="nav-link link d-none d-sm-none d-md-block reporteCompras" href="<?php echo SERVERURL; ?>reporteCompras/" id="" style="display:none"></i></div>Reporte Compras</a>
+        <a class="nav-link link d-none d-sm-none d-md-block cobrarClientes" href="<?php echo SERVERURL; ?>cobrarClientes/" id="cobrarClientes" style="display:none"><div class="sb-nav-link-icon"></div>CXC Clientes</a>
+        <a class="nav-link link d-none d-sm-none d-md-block pagarProveedores" href="<?php echo SERVERURL; ?>pagarProveedores/" id="pagarProveedores" style="display:none"><div class="sb-nav-link-icon"></div>CXP Proveedores</a>						
+        <a class="nav-link link d-none d-sm-none d-md-block inventario" href="<?php echo SERVERURL; ?>inventario/" id="inventario" style="display:none"><div class="sb-nav-link-icon"></div>Movimientos</a>
+        <a class="nav-link link d-none d-sm-none d-md-block transferencia" href="<?php echo SERVERURL; ?>transferencia/" id="transferencia" style="display:none"><div class="sb-nav-link-icon"></div>Inventario</a>
+  
+        <div class="dropdown d-block d-sm-block d-md-none">
+            <button class="btn btn-secondary bg-color-navarlateral dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Menú Rápido
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                
+                 <a class="dropdown-item reporteVentas" href="<?php echo SERVERURL; ?>reporteVentas/" id=""><div class="sb-nav-link-icon"></i></div>Reporte Ventas</a>
+                 <a class="dropdown-item reporteCotizacion" href="<?php echo SERVERURL; ?>reporteCotizacion/" id=""><div class="sb-nav-link-icon"></div>Reporte Cotización</a>
+                 <a class="dropdown-item reporteCompras" href="<?php echo SERVERURL; ?>reporteCompras/" id=""><div class="sb-nav-link-icon"></div>Reporte Compras</a>
+                <a class="dropdown-item cobrarClientes" href="<?php echo SERVERURL; ?>cobrarClientes/" id=""><div class="sb-nav-link-icon"></div>CXC Clientes</a>
+                <a class="dropdown-item pagarProveedores" href="<?php echo SERVERURL; ?>pagarProveedores/" id=""><div class="sb-nav-link-icon"></div>CXP Proveedores</a>						
+                <a class="dropdown-item inventario" href="<?php echo SERVERURL; ?>inventario/" id=""><div class="sb-nav-link-icon"></div>Movimientos</a>
+                <a class="dropdown-item transferencia" href="<?php echo SERVERURL; ?>transferencia/" id=""><div class="sb-nav-link-icon"></div>Inventario</a>
+            </div>
+        </div>    
+        
+
     <!-- Navbar-->      
     <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">  
         <li class="nav-item dropdown active">
